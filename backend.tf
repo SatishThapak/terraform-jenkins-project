7650f34d64906cc15b69fff4fd11ad91240507ae
satishthapak <satishthapak@outlook.com>
2025-06-29 14:46:23 +0530

    created backend,dynamodb,s3_

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-statefile-bucket-123456"     # your real bucket name
-    key            = "global/s3/terraform.tfstate"     # can change to suit project
-    region         = "ap-south-1"                       # match region
-    dynamodb_table = "terraform-locks"                 # name of your DynamoDB table
-    encrypt        = true
-  }
+    bucket         = "terraform-statefile-bucket-123456"     
+    key            = "s3/terraform.tfstate"     
+    region         = "ap-south-1"                       
+    dynamodb_table = "terraform-locks"               
+     }
 }
