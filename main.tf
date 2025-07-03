02ab40e97399c4c1e2c99de05303fcc0845d6118
satishthapak <satishthapak@outlook.com>
2025-07-03 14:58:27 +0530

    committed by satish

--- main.tf
+++ main.tf
@@ -35,7 +35,24 @@ module "load_balancer_target-group" {
   lb_target_group_port     = "8080"
   lb_target_group_protocol = "HTTP"
   vpc_id                   = module.networking.vpc_id
-  target_type              = "instance"
   jenkins_instance_id      = module.jenkins_instance.jenkins_instance_id
 }
 
+module "alb" {
+  source                    = "./load-balancer"
+  lb_name                   = "dev-proj-1-alb"
+  is_external               = false
+  lb_type                   = "application"
+  sg_enable_ssh_https       = module.security_group.sg_ec2_sg_ssh_http_id
+  subnet_ids                = tolist(module.networking.dev_proj_1_public_subnets)
+  tag_name                  = "dev-proj-1-alb"
+  lb_target_group_arn       = module.lb_target_group.dev_proj_1_lb_target_group_arn
+  ec2_instance_id           = module.jenkins.jenkins_ec2_instance_ip
+  lb_listner_port           = 80
+  lb_listner_protocol       = "HTTP"
+  lb_listner_default_action = "forward"
+  lb_https_listner_port     = 443
+  lb_https_listner_protocol = "HTTPS"
+  dev_proj_1_acm_arn        = module.aws_ceritification_manager.dev_proj_1_acm_arn
+  lb_target_group_attachment_port = 8080
+}
--- modules/load_balancer_target-group/variable.tf
+++ modules/load_balancer_target-group/variable.tf
@@ -1,6 +1,5 @@
 variable "lb_target_group_name" {}
 variable "lb_target_group_port" {}
 variable "lb_target_group_protocol" {}
-variable "target_type" {}
 variable "vpc_id" {}
 variable "jenkins_instance_id" {}
